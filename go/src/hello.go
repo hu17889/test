fd9bedba63b541a08dd70c5739f89ec95df574c0
root <root@li390-89.members.linode.com>
2012-12-10 05:54:01 -0500

    go test

--- go/src/hello.go
+++ go/src/hello.go
@@ -16,8 +16,20 @@ func main() {
     // fmt.Printf("%x",s)
 	// var s *stack.Stack
 	// s := new(st.Stack)
-	s := st.NewStack()
-	s.Push(1)
-	s.Push(2)
+	// s := st.NewStack()
+	// s.Push(1)
+	// s.Push(2)
+	// s.Pushs(3,4,5)
+    // fmt.Printf("%v",*s)
+	// ret,err := s.Pop()
+	s := st.NewDoStack()
+	println(s)
     fmt.Printf("%v",*s)
+	// ret,err = s.Pop()
+	// println(ret,err)
+	// ret,err = s.Pop()
+	// println(ret,err.Error())
+	// fmt.Print(err)
+    // fmt.Printf("%v",*s)
+
 }

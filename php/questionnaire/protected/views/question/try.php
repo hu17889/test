2fe6c27559bd2feb182cc24767fd094c3d152939
hucong <hucong@360.cn>
2013-01-04 18:39:05 +0800

    question

---
+++ php/questionnaire/protected/views/question/try.php
@@ -0,0 +1,13 @@
+<div id="info">
+<div class="info_pannel">
+    <table>
+    <tr> <th></th> <th>R1</th> <th>R2</th> </tr>
+    <tr> <th>C1</th> <td>My first HTML</td> <td>$53</td> </tr>
+    <tr> <th>C2</th> <td>My first HTML</td> <td>$53</td> </tr>
+    </table>
+</div>
+<div class="timer">
+</div>
+</div>
+<div class="question_pannel">
+</div>

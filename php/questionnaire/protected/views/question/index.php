e8a6191d1122326ab1030becc06e6920b0dd981b
hucong <hucong@360.cn>
2013-01-08 12:02:27 +0800

    question

--- php/questionnaire/protected/views/question/index.php
+++ php/questionnaire/protected/views/question/index.php
@@ -1,5 +1,5 @@
 <div id="welcome">
-<div class="content">欢迎界面</div>
+<div class="content">感谢您参与应急情境下应对决策能力的研究。该研究为哈尔滨工业大学应急决策研究项目中的一部分，本研究为不记名形式，需要每位参与者独立完成，调查问卷的完成大概需要5~10分钟。所有问卷的信息质为本项目研究之用，在你填写问卷的过程当中如有疑问，可以向您周围的研究员提问，或者联系我们的研究员E-mail：yuminglu1983@163.com, 电话：18646048335。再次感谢您的参与。</div>
 <form action="/question/try" id="questionnaire">
     <input type="hidden" name="naireid" value="<?php echo htmlspecialchars($naireid);?>"/>
     <input type="submit" value="开始" class="nextpage"/>

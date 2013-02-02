81bfb3c40c2872ab3891f7da2dd10d8f3e0e3468
hucong <hucong@360.cn>
2013-02-02 18:31:54 +0800

    change question

--- php/questionnaire/protected/views/question/index.php
+++ php/questionnaire/protected/views/question/index.php
@@ -1,5 +1,8 @@
 <div id="index">
+<button id="test">next</button>
 <form action="/question/welcome" id="questionnaire">
     <input type="submit" value="开始" class="nextpage"/>
 </form>
 </div>
+<script type="text/javascript">
+</script>

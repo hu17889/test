a75c8a0cb3570ecb55f7f89cee81081d8ddab9ae
hucong <hucong@360.cn>
2013-03-31 16:23:42 +0800

    chi

--- php/questionnaire/protected/views/question/chi/questions_3.php
+++ php/questionnaire/protected/views/question/chi/questions_3.php
@@ -1,11 +1,11 @@
-    <h4> 回想一下在应对这一突发事件时您的感觉，请作答。</h4>
+﻿    <h4> 回想一下在应对这一突发事件时您的感觉，请作答。</h4>
 <h4><a name="q1">（1）果断的</a></h4>
     <div class="answer"> <p>
     <input type="radio" name="q1" value="1"/>A. 完全没有
     <input type="radio" name="q1" value="2"/>B. 有一点
     <input type="radio" name="q1" value="3"/>C. 有一些
     <input type="radio" name="q1" value="4"/>D. 有很多
-    <input type="radio" name="q1" value="5"/>D. 一直有
+    <input type="radio" name="q1" value="5"/>E. 一直有
     </p> </div>
 <h4><a name="q2">（2）不安的</a></h4>
     <div class="answer"> <p>
@@ -13,7 +13,7 @@
     <input type="radio" name="q2" value="2"/>B. 有一点
     <input type="radio" name="q2" value="3"/>C. 有一些
     <input type="radio" name="q2" value="4"/>D. 有很多
-    <input type="radio" name="q2" value="5"/>D. 一直有
+    <input type="radio" name="q2" value="5"/>E. 一直有
     </p> </div>
 <h4><a name="q3">（3）害怕的</a></h4>
     <div class="answer"> <p>
@@ -21,7 +21,7 @@
     <input type="radio" name="q3" value="2"/>B. 有一点
     <input type="radio" name="q3" value="3"/>C. 有一些
     <input type="radio" name="q3" value="4"/>D. 有很多
-    <input type="radio" name="q3" value="5"/>D. 一直有
+    <input type="radio" name="q3" value="5"/>E. 一直有
     </p> </div>
 <h4><a name="q4">（4）抵触的</a></h4>
     <div class="answer"> <p>
@@ -29,7 +29,7 @@
     <input type="radio" name="q4" value="2"/>B. 有一点
     <input type="radio" name="q4" value="3"/>C. 有一些
     <input type="radio" name="q4" value="4"/>D. 有很多
-    <input type="radio" name="q4" value="5"/>D. 一直有
+    <input type="radio" name="q4" value="5"/>E. 一直有
     </p> </div>
 <h4><a name="q5">（5）细心的</a></h4>
     <div class="answer"> <p>
@@ -37,7 +37,7 @@
     <input type="radio" name="q5" value="2"/>B. 有一点
     <input type="radio" name="q5" value="3"/>C. 有一些
     <input type="radio" name="q5" value="4"/>D. 有很多
-    <input type="radio" name="q5" value="5"/>D. 一直有
+    <input type="radio" name="q5" value="5"/>E. 一直有
     </p> </div>
 <h4><a name="q6">（6）警觉的</a></h4>
     <div class="answer"> <p>
@@ -45,7 +45,7 @@
     <input type="radio" name="q6" value="2"/>B. 有一点
     <input type="radio" name="q6" value="3"/>C. 有一些
     <input type="radio" name="q6" value="4"/>D. 有很多
-    <input type="radio" name="q6" value="5"/>D. 一直有
+    <input type="radio" name="q6" value="5"/>E. 一直有
     </p> </div>
 <h4><a name="q7">（7）愧疚的</a></h4>
     <div class="answer"> <p>
@@ -53,7 +53,7 @@
     <input type="radio" name="q7" value="2"/>B. 有一点
     <input type="radio" name="q7" value="3"/>C. 有一些
     <input type="radio" name="q7" value="4"/>D. 有很多
-    <input type="radio" name="q7" value="5"/>D. 一直有
+    <input type="radio" name="q7" value="5"/>E. 一直有
     </p> </div>
 <h4><a name="q8">（8）鼓舞的</a></h4>
     <div class="answer"> <p>
@@ -61,7 +61,7 @@
     <input type="radio" name="q8" value="2"/>B. 有一点
     <input type="radio" name="q8" value="3"/>C. 有一些
     <input type="radio" name="q8" value="4"/>D. 有很多
-    <input type="radio" name="q8" value="5"/>D. 一直有
+    <input type="radio" name="q8" value="5"/>E. 一直有
     </p> </div>
 <h4><a name="q9">（9）紧张的</a></h4>
     <div class="answer"> <p>
@@ -69,7 +69,7 @@
     <input type="radio" name="q9" value="2"/>B. 有一点
     <input type="radio" name="q9" value="3"/>C. 有一些
     <input type="radio" name="q9" value="4"/>D. 有很多
-    <input type="radio" name="q9" value="5"/>D. 一直有
+    <input type="radio" name="q9" value="5"/>E. 一直有
     </p> </div>
 <h4><a name="q10">（10）积极的</a></h4>
     <div class="answer"> <p>
@@ -77,5 +77,5 @@
     <input type="radio" name="q10" value="2"/>B. 有一点
     <input type="radio" name="q10" value="3"/>C. 有一些
     <input type="radio" name="q10" value="4"/>D. 有很多
-    <input type="radio" name="q10" value="5"/>D. 一直有
+    <input type="radio" name="q10" value="5"/>E. 一直有
     </p> </div>

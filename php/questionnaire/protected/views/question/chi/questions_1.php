e40d4bb7337a42ab75365dc78de8562178de2532
root <root@li390-89.members.linode.com>
2014-08-17 03:59:37 -0400

    change

--- php/questionnaire/protected/views/question/chi/questions_1.php
+++ php/questionnaire/protected/views/question/chi/questions_1.php
@@ -46,8 +46,14 @@
     <h4><a name="q6">6. 在您做决策时，您是如何处理“备用电源”因素中“一段时间”“较长时间”这样的信息的？</a></h4>
     <div class="answer"> <p>
     <input type="radio" name="t6" value="1"/>A.等待得到了确切信息
-    <input type="radio" name="t6" value="2"/>B.自己估计了一下确切数值
+</p>
+<p>
+    <input type="radio" name="t6" value="2"/>B.自己估计了一下确切数值,并请填入估计值(<input type="text">--<input type="text">)
+</p>
+<p>
     <input type="radio" name="t6" value="3"/>C.自己大概估计了一下，但没用确切数值
+</p>
+<p>
     <input type="radio" name="t6" value="4"/>D.直接忽略这样的信息
     </p> </div>
 	<?php }?>
